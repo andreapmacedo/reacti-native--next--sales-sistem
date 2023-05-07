b533330d86ce6212173de5e694972afbf893ab27
andreapmacedo <aa.andremacedo@gmail.com>
2023-05-07 20:49:00 -0300

    Feat: s03-a026 - Aula useState.

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import React, { useState } from "react";
 
 interface ExampleProps {
   text?: string;
@@ -8,13 +9,24 @@ interface ExampleProps {
 
 // const Example = (props: ExampleProps) => {
 const Example = ({children, text, textChange: newPropName}: ExampleProps) => {
+  
+  const [newText, setnewText] = useState<string>('oia');
+
+  const handleOnPress = () => {
+    setnewText('novo texto');
+    console.log('Clicou');
+  }
+  
   return (
     <View>
-      <Text style={{color: 'red'}} >{children}</Text>
-      <Text style={{color: 'green'}} >{text}</Text>
-      <Text style={{color: 'blue'}} >{newPropName}</Text>
+      {/* <Text style={{color: 'blue'}} >{newPropName}</Text> */}
       {/* <Text style={{color: 'red'}} >{props.children}</Text>
       <Text style={{color: 'green'}} >{props.text}</Text> */}
+      <Text style={{color: 'red'}} >{children}</Text>
+      <Text style={{color: 'green'}} >{text}</Text>
+      <Text style={{color: 'blue'}} >{newPropName || text}</Text>
+      <Text style={{color: 'purple'}} >{newText}</Text>
+      <Button title="Clique aqui" onPress={handleOnPress} />
     </View>
   );
 };

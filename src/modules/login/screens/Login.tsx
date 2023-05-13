52559358a3f80fdd2f027f1b12e432abbe62ba7f
andreapmacedo <aa.andremacedo@gmail.com>
2023-05-13 17:59:47 -0300

    Feat: s06-a050 - criando componente de icone.

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -3,6 +3,7 @@ import { ContainerLogin } from "../styles/login.style";
 import Input from "../../../shared/components/input/Input";
 import Button from "../../../shared/components/button/Button";
 import { theme } from "../../../shared/themes/theme";
+import { Icon } from "../../../shared/components/icon/icon";
 
 const Login = () => {
   const handleOnPress = () => {
@@ -11,6 +12,8 @@ const Login = () => {
   return (
     <View>
       <ContainerLogin>
+        <Icon name="home3" size={44} color="blue"/>
+        {/* <Icon name="home3" /> */}
         <Input 
           title="email:"
           placeholder="Digite seu email"
